8b9751e717a934684792db920833047ca85122c1
Sumit Anand <anandsumit2000@gmail.com>
2024-02-03 12:30:43 +0530

    setting up modules for eks and vpc with base-config.

---
+++ aws-eks/main.tf
@@ -0,0 +1,4 @@
+resource "aws_eks_cluster" "cluster" {
+  name=var.cluster_name
+  
+}
---
+++ aws-vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type = "string"
+}
+
+variable "region" {
+  
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "subnet_cidr" {
+  
+}
+
+variable "availability_zone" {
+  type = list()
+}
